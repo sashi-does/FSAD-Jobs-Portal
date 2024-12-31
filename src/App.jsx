a2eecdd141dca63362cd1be93b2da2258b44f04e
sashi-does <sashiontop@gmail.com>
2024-12-31 12:24:53 +0530

    Modified Home Route

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,17 @@
-import React, { Component } from 'react'
-
-export default class App extends Component {
-  render() {
-    return (
-      <div>
-        <div id='header'>
-          <h1>Header</h1>
-        </div>
-        <div id='content'>
-          <h1>Content</h1>
-        </div>
-        <div id='footer'>
-          <h1>Footer</h1>
-        </div>
-      </div>
-    );
-  }
-}
+import React, { Component } from 'react'; 
+import './App.css'; 
+ 
+class App extends Component { 
+    render() { 
+        return ( 
+            <div id='container'> 
+                <div id='header'>Header</div> 
+                <div id='content'>Content</div> 
+                <div id='footer'>Footer</div> 
+                 
+            </div> 
+        ); 
+    } 
+} 
+ 
+export default App;
