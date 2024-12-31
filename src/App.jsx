d99651f7a716986c2eca79021a96aeedb58d5ebc
sashi-does <sashiontop@gmail.com>
2024-12-31 11:52:55 +0530

    changed app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,15 @@ export default class App extends Component {
   render() {
     return (
       <div>
-        <h1>Welcome to Jobs Portal</h1>
+        <div id='header'>
+          <h1>Header</h1>
+        </div>
+        <div id='content'>
+          <h1>Content</h1>
+        </div>
+        <div id='footer'>
+          <h1>Footer</h1>
+        </div>
       </div>
     );
   }
