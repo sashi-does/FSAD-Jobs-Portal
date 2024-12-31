7857976c22257f8307b9cba412c143d86f590d0b
sashi-does <sashiontop@gmail.com>
2024-12-31 12:40:29 +0530

    Added Styels

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
 import React, { Component } from 'react'; 
+import logo from '../public/logo.png';
 import './App.css'; 
  
 class App extends Component { 
     render() { 
         return ( 
             <div id='container'> 
-                <div id='header'>Header</div> 
+                <div id='header'>
+                  <img src={logo} className='logo' alt='logo' />
+                  <div className='logo-text'>
+                    <span className='logo-text-1'>Jobs</span>Portal
+                  </div>
+                </div> 
                 <div id='content'>Content</div> 
                 <div id='footer'>Footer</div> 
-                 
             </div> 
         ); 
     } 
