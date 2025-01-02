6cadf75268ed42a40853350c97fd76c27ee89ece
sashi-does <sashiontop@gmail.com>
2025-01-02 11:03:53 +0530

    modified

--- src/App.jsx
+++ src/App.jsx
@@ -10,8 +10,22 @@ class App extends Component {
                 <img className='signinIcon' src="../public/user.png" alt="sign in icon" />
                 <label className='signinText'>Sign In</label>
                 </div>
-               <div id="content">Content</div>
-               <div id="footer">Fotter</div>
+               <div id="content">
+                <div className='text1'>India's No.1 Job Portal</div>
+                <div className='text2'>Your job search ends here</div>
+                <div className='text3'>discover career opportunities</div>
+                <div className='searchBar'>
+                  <input type="text" className='searchjobText' placeholder='Search for jobs' />
+                  <input type="text" className='joblocationText' value="Search" />
+                  <button className='searchjob'>Search Jobs</button>
+                </div>
+               </div>
+               <div id="footer">
+                <label className='copyrightText'>Copyright 2025 Jobs Portal. All rights reserved.</label>
+                <img className='socialmediaIcon' src="facebook.png" alt="facebook icon" />
+                <img className='socialmediaIcon' src="twitter.png" alt="twitter icon" />
+                <a href="https://www.linkedin.com/in/sakshi-gupta-1234567890/"><img className='socialmediaIcon' src="linkedin.png" alt="linkedin icon" /> </a>
+               </div>
             </div>
         );
     }
